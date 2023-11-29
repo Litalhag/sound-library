68891b366eade8b7ef6231651a1019424b9daddc
Litalhag <litalhag@gmail.com>
2023-11-29 11:07:16 +0200

    firebase config

--- src/googleSigning/config.js
+++ src/googleSigning/config.js
@@ -3,8 +3,11 @@ import { getAnalytics } from 'firebase/analytics'
 
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY
+
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey,
   authDomain: 'sound-library-abb03.firebaseapp.com',
   projectId: 'sound-library-abb03',
   storageBucket: 'sound-library-abb03.appspot.com',
